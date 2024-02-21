e7613875865f4fb5281cb71f89a73da6ef2ade0f
Craig Thacker <craigthackerx@gmail.com>
2024-02-21 21:37:58 +0000

    Update

--- containers/alpine/start.sh
+++ containers/alpine/start.sh
@@ -12,7 +12,7 @@ if [ -z "${AZP_TOKEN_FILE}" ]; then
     exit 1
   fi
 
-  AZP_TOKEN_FILE="/azp/.token"
+  AZP_TOKEN_FILE="${AZP_DIRECTORY}/.token"
   echo -n "${AZP_TOKEN}" > "${AZP_TOKEN_FILE}"
 fi
 
