71ad6b5a15f4e492ce25ab3f2c5d9594d55fd4b3
Craig Thacker <craigthackerx@gmail.com>
2024-02-22 16:57:54 +0000

    Update

--- Run-Docker.ps1
+++ Run-Docker.ps1
@@ -132,7 +132,7 @@ function Build-DockerImage
     try
     {
         Write-Host "Info: Building Docker image $DockerImageName from $filePath" -ForegroundColor Green
-        docker build -t $DockerImageName -f $filePath $Path | Out-Host
+        docker build --squash -t $DockerImageName -f $filePath $Path | Out-Host
         if ($LASTEXITCODE -eq 0)
         {
             return $true
