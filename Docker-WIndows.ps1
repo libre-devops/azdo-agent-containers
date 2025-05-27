8c3c2666818bafa416bff4f2e87126eb52df6cdb
Craig Thacker <craigthackerx@gmail.com>
2025-05-27 01:10:14 +0100

    update script

--- Docker-WIndows.ps1
+++ Docker-WIndows.ps1
@@ -1,9 +1,5 @@
 docker run -it --rm `
-    -e AZP_URL `
-    -e AZP_TOKEN `
-    -v "//var/run/docker.sock:/var/run/docker.sock" `
-    ghcr.io/libre-devops/azdo-agent-containers/ubuntu:latest `
-    -NoLogo -NoProfile -Command {
-    "AZP_URL   = $env:AZP_URL"
-    "AZP_TOKEN = $env:AZP_TOKEN"
-}
+   -e AZP_URL=$Env:AZP_URL `
+   -e AZP_TOKEN=$Env:AZP_TOKEN `
+   -v "//var/run/docker.sock:/var/run/docker.sock" `
+   ghcr.io/libre-devops/azdo-agent-containers/ubuntu:latest
